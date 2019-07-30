3d8634fe775f8e72c241fd389d56c8f54fb5df6e
Mike Ross <34175985+boywonderstrikesback@users.noreply.github.com>
2019-07-30 13:16:41 -0400

    Add files via upload

---
+++ DYnamic/DYnamic/main.m
@@ -0,0 +1,67 @@
+//
+//  main.m
+//  DYnamic
+//
+//  Created by Mike Ross on 7/30/19.
+//  Copyright © 2019 Mike Ross. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Foundation/Foundation.h>
+
+@interface Square:NSObject {
+    float area;
+}
+
+- (void)calculateAreaOfSide:(CGFloat)side;
+- (void)printArea;
+@end
+
+@implementation Square
+- (void)calculateAreaOfSide:(CGFloat)side {
+    area = side * side;
+}
+
+- (void)printArea {
+    NSLog(@"The area of square is %f",area);
+}
+
+@end
+
+@interface Rectangle:NSObject {
+    float area;
+}
+
+- (void)calculateAreaOfLength:(CGFloat)length andBreadth:(CGFloat)breadth;
+- (void)printArea;
+@end
+
+@implementation  Rectangle
+
+- (void)calculateAreaOfLength:(CGFloat)length andBreadth:(CGFloat)breadth {
+    area = length * breadth;
+}
+
+- (void)printArea {
+    NSLog(@"The area of Rectangle is %f",area);
+}
+
+@end
+
+int main() {
+    Square *square = [[Square alloc]init];
+    [square calculateAreaOfSide:10.0];
+    
+    Rectangle *rectangle = [[Rectangle alloc]init];
+    [rectangle calculateAreaOfLength:10.0 andBreadth:5.0];
+    
+    NSArray *shapes = [[NSArray alloc]initWithObjects: square, rectangle,nil];
+    id object1 = [shapes objectAtIndex:0];
+    [object1 printArea];
+    
+    id object2 = [shapes objectAtIndex:1];
+    [object2 printArea];
+    
+    return 0;
+}
